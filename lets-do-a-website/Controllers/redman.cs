a833740071820fcb0fc10f24e4f8b79279373488
LetsDoEverythingYT <vbcpp.games@gmail.com>
2024-02-04 15:30:10 -0500

    Redman books

---
+++ lets-do-a-website/Controllers/redman.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace lets_do_a_website.Controllers
+{
+    public class redman : Controller
+    {
+        public IActionResult Index(int id=1)
+        {
+            ViewData["bookId"] = id;
+            return View();
+        }
+    }
+}
